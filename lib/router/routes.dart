324b2cb4e2e26514e69cd1d9faf7a927d8793c06
shin-SeungHun <sng7387@gmail.com>
2024-03-28 21:10:30 +0900

    refactor: router에 getBuilder 적용

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,17 +1,19 @@
 import 'package:flutter_status_management/di/di_setup.dart';
 import 'package:flutter_status_management/ui/main/main_screen.dart';
 import 'package:flutter_status_management/ui/main/main_view_model.dart';
+import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 final router = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => ChangeNotifierProvider(
-        create: (context) => getIt<MainViewModel>(),
-        child: const MainScreen(),
+      builder: (context, state) => GetBuilder(
+        init: getIt<MainViewModel>(),
+        builder: (controller) => MainScreen(
+          viewModel: Get.put(getIt<MainViewModel>()),
+        ),
       ),
     ),
   ],
