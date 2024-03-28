93bd3a0b686c5b197c9681a2cf51c8d48f85d0d2
shin-SeungHun <sng7387@gmail.com>
2024-03-28 15:06:02 +0900

    refactor: map 대신 dto 타입

--- lib/data/data_source/image_api.dart
+++ lib/data/data_source/image_api.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 
+import 'package:flutter_status_management/data/dto/image_dto.dart';
 import 'package:http/http.dart' as http;
 
 class ImageApi {
-  Future<Map<String, dynamic>> getImageInfoJson({required String query}) async {
+  Future<ImageDto> getImageInfoJson({required String query}) async {
     final response = await http.get(Uri.parse(
         'https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query&image_type=photo'));
     return jsonDecode(response.body);
