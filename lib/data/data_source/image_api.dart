3e56cd4e8359549caba94975ceea5cac7a5fd6e1
shin-SeungHun <sng7387@gmail.com>
2024-03-28 15:36:00 +0900

    refactor: return 값 수정

--- lib/data/data_source/image_api.dart
+++ lib/data/data_source/image_api.dart
@@ -4,9 +4,9 @@ import 'package:flutter_status_management/data/dto/image_dto.dart';
 import 'package:http/http.dart' as http;
 
 class ImageApi {
-  Future<ImageDto> getImageInfoJson({required String query}) async {
+  Future<ImageDto> getImageResult({required String query}) async {
     final response = await http.get(Uri.parse(
         'https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query&image_type=photo'));
-    return jsonDecode(response.body);
+    return ImageDto.fromJson(jsonDecode(response.body));
   }
 }
