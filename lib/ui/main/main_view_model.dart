6959e8b356b11891bafee62ce9bf9c9feacbd86c
shin-SeungHun <sng7387@gmail.com>
2024-03-28 21:10:54 +0900

    refactor: GetxController 상속

--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_status_management/data/model/image_model.dart';
 import 'package:flutter_status_management/data/repository/image_item_repository.dart';
+import 'package:get/get.dart';
 
-class MainViewModel extends ChangeNotifier {
-
+class MainViewModel extends GetxController {
   final ImageItemRepository repository;
   final TextEditingController textEditingController = TextEditingController();
 
@@ -12,23 +12,24 @@ class MainViewModel extends ChangeNotifier {
   });
 
   @override
-  void dispose() {
-    super.dispose();
+  void onClose() {
+    super.onClose();
     textEditingController.dispose();
   }
 
   List<ImageModel> imageList = [];
 
-  bool isLoading = false;
+  RxBool isLoading = false.obs;
 
   Future<void> searchImage({required String query}) async {
-    isLoading = true;
-    notifyListeners();
+    isLoading.value = true;
 
-    imageList = await repository.getImageItems(query: query);
-    isLoading = false;
-    notifyListeners();
+    try {
+      imageList = await repository.getImageItems(query: query);
+      update();
+    } finally {
+      isLoading.value = false;
+      update();
+    }
   }
-
-
 }
