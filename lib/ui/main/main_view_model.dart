5f908e8f406f1e33dc42e2601cf7d165359cfbcd
shin-SeungHun <sng7387@gmail.com>
2024-04-01 22:47:52 +0900

    refactor: 생성자에서 on() 메소드를 사용해 로직 처리

--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -1,35 +1,26 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_status_management/data/model/image_model.dart';
 import 'package:flutter_status_management/data/repository/image_item_repository.dart';
-import 'package:get/get.dart';
+import 'package:flutter_status_management/event/search_image_event.dart';
+import 'package:flutter_status_management/event/search_image_event_extends.dart';
 
-class MainViewModel extends GetxController {
+class MainViewModel extends Bloc<SearchImageEvent, List<ImageModel>> {
   final ImageItemRepository repository;
-  final TextEditingController textEditingController = TextEditingController();
 
-  MainViewModel({
-    required this.repository,
-  });
+  // final TextEditingController textEditingController = TextEditingController();
 
-  @override
-  void onClose() {
-    super.onClose();
-    textEditingController.dispose();
+  MainViewModel({required this.repository}) : super([]) {
+    on<SearchImageEventExtends>((event, emit) async {
+      emit(await _searchImage(event.query));
+    });
   }
 
-  List<ImageModel> imageList = [];
-
-  RxBool isLoading = false.obs;
-
-  Future<void> searchImage({required String query}) async {
-    isLoading.value = true;
-
+  Future<List<ImageModel>> _searchImage(String query) async {
     try {
-      imageList = await repository.getImageItems(query: query);
-      update();
-    } finally {
-      isLoading.value = false;
-      update();
+      return await repository.getImageItems(query: query);
+    } catch (e) {
+      // 에러 처리
+      return [];
     }
   }
 }
