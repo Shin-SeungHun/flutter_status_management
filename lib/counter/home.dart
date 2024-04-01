10962c8ef581c92942f3c95e13b1761b3c682d16
shin-SeungHun <sng7387@gmail.com>
2024-04-01 17:41:18 +0900

    feat: .add()로 상태관리 적용

--- lib/counter/home.dart
+++ lib/counter/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_status_management/counter/count_cubit.dart';
+import 'package:flutter_status_management/counter/count_bloc.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,10 +9,10 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('cubit 상태관리'),
+        title: const Text('bloc 상태관리'),
       ),
       body: Center(
-        child: BlocBuilder<CountCubit, int>(builder: (context, state) {
+        child: BlocBuilder<CountBloc, int>(builder: (context, state) {
           return Text(
             state.toString(),
             style: const TextStyle(fontSize: 80),
@@ -24,13 +24,13 @@ class Home extends StatelessWidget {
         children: [
           FloatingActionButton(
             onPressed: () {
-              context.read<CountCubit>().addCount();
+              context.read<CountBloc>().add(AddCountEvent());
             },
             child: const Icon(Icons.add),
           ),
           FloatingActionButton(
             onPressed: () {
-              context.read<CountCubit>().substractCount();
+              context.read<CountBloc>().add(substractCountEvent());
             },
             child: const Icon(Icons.minimize),
           ),
