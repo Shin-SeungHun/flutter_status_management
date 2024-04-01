8997ab78f411e8522f672585980181f841017cec
shin-SeungHun <sng7387@gmail.com>
2024-04-01 22:40:41 +0900

    refactor: 라우터에 observer 클래스 추가, BlocProvider 연결

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,21 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_status_management/di/di_setup.dart';
+import 'package:flutter_status_management/observer/router_observer.dart';
 import 'package:flutter_status_management/ui/main/main_screen.dart';
 import 'package:flutter_status_management/ui/main/main_view_model.dart';
-import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
   initialLocation: '/',
+  observers: [RouterObserver()],
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => GetBuilder(
-        init: getIt<MainViewModel>(),
-        dispose: (state) {
-          Get.delete<MainViewModel>();
-        },
-        builder: (controller) => const MainScreen(),
-      ),
+      builder: (context, state) =>
+          BlocProvider(create: (context) => getIt<MainViewModel>(), lazy: false, child: const MainScreen()),
     ),
   ],
 );
